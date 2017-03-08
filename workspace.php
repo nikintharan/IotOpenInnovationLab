4f9eea024befa9b61b6be2dd3036357bd524e2bc
Nikin Tharan <tharan.n@husky.neu.edu>
2017-03-08 22:42:49 +0000

    removed breadcrumbs

--- workspace.php
+++ workspace.php
@@ -4,10 +4,6 @@
     <div class="page-header">      
       <div class="container"> 
         <div class="page-header-inner">         
-          <ol class="breadcrumb wow fadeInDown" data-wow-delay="300ms">
-            <li><a href="index.php">Home</a></li>
-            <li class="page">Start</li>
-          </ol>
           <h1 class="page-title wow fadeInRight" data-wow-delay="300ms">
            Workspace
           </h1>
